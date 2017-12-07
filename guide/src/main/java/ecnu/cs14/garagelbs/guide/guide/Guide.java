e713378be565bab079e5f04cdac9f8a1f1f3f19d
K <hpkelvin@163.com>
2017-12-07 16:09:47 +0800

    Guide class as a controlling unit.

---
+++ guide/src/main/java/ecnu/cs14/garagelbs/guide/guide/Guide.java
@@ -0,0 +1,71 @@
+package ecnu.cs14.garagelbs.guide.guide;
+
+import android.content.Context;
+import com.onlylemi.mapview.library.MapView;
+import com.onlylemi.mapview.library.layer.*;
+
+/**
+ * Controlling.
+ * Created by kel on 12/7/17.
+ */
+
+public class Guide {
+    /**
+     * Initializing.
+     */
+    void initialize(
+            Context context,
+            MapView mapView,
+            MapLayer mapLayer,
+            LocationLayer locationLayer,
+            MarkLayer markLayer,
+            BitmapLayer bitmapLayer,
+            RouteLayer routeLayer
+    ) {
+        // start location updating
+    }
+
+    void finialize() {
+        // stop location updating
+    }
+
+    void updateLocation() {
+        // invoked by the child thread to update
+    }
+
+    /**
+     * Mark current destination.
+     */
+    void markDestination() {
+
+    }
+
+    /**
+     * Change the destination to the given node.
+     * @param nodeID The target node ID.
+     */
+    void updateDestination(int nodeID) {
+
+    }
+
+    /**
+     * Remove the destination. Note: It changes status.
+     */
+    void removeDestination() {
+
+    }
+
+    /**
+     * Start guiding.
+     */
+    void startGuiding() {
+
+    }
+
+    /**
+     * Finish guiding.
+     */
+    void finishGuiding() {
+
+    }
+}
