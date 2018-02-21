5536adcd538d8e99da469520dad2028bed996bac
K <hpkelvin@163.com>
2018-02-21 17:05:40 +0800

    Base of the guide module. TestData. Destination related codes and UIs.

--- guide/src/main/java/ecnu/cs14/garagelbs/guide/guide/Guide.java
+++ guide/src/main/java/ecnu/cs14/garagelbs/guide/guide/Guide.java
@@ -1,9 +1,14 @@
 package ecnu.cs14.garagelbs.guide.guide;
 
 import android.content.Context;
-import com.onlylemi.mapview.library.MapView;
-import com.onlylemi.mapview.library.layer.*;
+import android.graphics.PointF;
+import com.onlylemi.mapview.library.utils.MapMath;
+import ecnu.cs14.garagelbs.support.data.Position;
+import ecnu.cs14.garagelbs.support.locating.DummyAlgorithm;
 import ecnu.cs14.garagelbs.support.locating.Locator;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 /**
  * Controlling.
@@ -11,34 +16,115 @@ import ecnu.cs14.garagelbs.support.locating.Locator;
  */
 
 public class Guide {
+
+    private Position location = null;
+//    private Position destination = null;
+    private int destinationNodeId = -1;
+
+    private List<PointF> nodesList = null;
+
+    private WeakReference<MainActivity> activityRef;
+
+    final static int NOT_INITIALIZED_STATUS = -1;
+    final static int STANDBY_STATUS = 0;
+    final static int MARKED_STATUS = 1;
+    final static int GUIDING_STATUS = 2;
+    int status = NOT_INITIALIZED_STATUS;
+
+    private class UpdatingThread extends Thread {
+
+        boolean running = false;
+        Locator locator = null;
+        private Context context;
+
+        public UpdatingThread(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            try {
+                locator = new Locator(context, DummyAlgorithm.class);
+//                locator = new Locator(context, AlgorithmImpl.class);
+                context = null;
+                running = true;
+            } catch (Exception e) {
+                running = false;
+                e.printStackTrace();
+            }
+            Position position;
+            while (running) {
+                position = locator.locate();
+//                synchronized (Guide.this) {
+                    Guide.this.updateLocation(position);
+//                }
+            }
+        }
+    }
+
+    private UpdatingThread thread = null;
+
     /**
      * Initializing.
      */
     void initialize(
-            Context context,
-            MapView mapView,
-            MapLayer mapLayer,
-            LocationLayer locationLayer,
-            MarkLayer markLayer,
-            BitmapLayer bitmapLayer,
-            RouteLayer routeLayer
+            MainActivity activity
+//            MapView mapView,
+//            MapLayer mapLayer,
+//            LocationLayer locationLayer,
+//            MarkLayer markLayer,
+//            BitmapLayer bitmapLayer,
+//            RouteLayer routeLayer
     ) {
+        activityRef = new WeakReference<>(activity);
+
         // start location updating
+        thread = new UpdatingThread(activity);
+        thread.start();
+
+        nodesList = TestData.getNodesList();
+
+        status = STANDBY_STATUS;
     }
 
     void finialize() {
         // stop location updating
+        thread.running = false;
     }
 
-    void updateLocation() {
+    private int findClosestNode(PointF point) {
+        float distance = Float.MAX_VALUE;
+        int index = -1;
+        for (int i=0; i < nodesList.size(); i++) {
+            float d = MapMath.getDistanceBetweenTwoPoints(nodesList.get(i), point);
+            if (d < distance) {
+                distance = d;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private int findClosestNode(Position position) {
+        return findClosestNode(new PointF(position.x, position.y));
+    }
+
+    void updateLocation(Position position) {
         // invoked by the child thread to update
+        this.location = position;
     }
 
     /**
      * Mark current destination.
      */
     void markDestination() {
-
+        if (status != STANDBY_STATUS) {
+            return;
+        }
+        status = MARKED_STATUS;
+//        destination = location;
+        updateDestination(findClosestNode(location));
     }
 
     /**
@@ -46,7 +132,11 @@ public class Guide {
      * @param nodeID The target node ID.
      */
     void updateDestination(int nodeID) {
-
+        if (status != MARKED_STATUS) {
+            return;
+        }
+        destinationNodeId = nodeID;
+        activityRef.get().markDestination(nodesList.get(nodeID));
     }
 
     /**
--- guide/src/main/java/ecnu/cs14/garagelbs/guide/guide/MainActivity.java
+++ guide/src/main/java/ecnu/cs14/garagelbs/guide/guide/MainActivity.java
@@ -1,22 +1,36 @@
 package ecnu.cs14.garagelbs.guide.guide;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.onlylemi.mapview.library.MapView;
+import com.onlylemi.mapview.library.MapViewListener;
+import com.onlylemi.mapview.library.layer.BitmapLayer;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getCanonicalName();
+
+    private Guide guide = new Guide();
+    private MapView mapView = null;
+    private BitmapLayer bitmapLayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +38,49 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+//                markDestination(new PointF(200, 200));
             }
         });
+
+        mapView = findViewById(R.id.mapview);
+        mapView.setMapViewListener(new MapViewListener() {
+            @Override
+            public void onMapLoadSuccess() {
+                Bitmap bmp = null;
+                try {
+                    bmp = BitmapFactory.decodeStream(getAssets().open("car.png"));
+                } catch (IOException e) {
+                    Log.e(TAG, "onMapLoadSuccess: loading bitmap mark failed", e);
+                    e.printStackTrace();
+                }
+                bitmapLayer = new BitmapLayer(mapView, bmp);
+                bitmapLayer.isVisible = false;
+                mapView.addLayer(bitmapLayer);
+
+                mapView.refresh();
+            }
+
+            @Override
+            public void onMapLoadFail() {
+
+            }
+        });
+        Bitmap bitmap;
+        try {
+            bitmap = BitmapFactory.decodeStream(getAssets().open("map.png"));
+            mapView.loadMap(bitmap);
+        } catch (IOException e) {
+            Log.e(TAG, "onCreate: loading map image failed", e);
+            e.printStackTrace();
+        }
+
+        guide.initialize(this);
+    }
+
+    public void markDestination(PointF destination) {
+        bitmapLayer.setLocation(destination);
+        bitmapLayer.isVisible = true;
+        mapView.refresh();
     }
 
     @Override
